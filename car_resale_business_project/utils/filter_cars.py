764aa3a36efd58d2a7b543be1fc70789eae56348
Mykhailo20 <myhailo20189@gmail.com>
2024-04-21 18:57:23 +0300

    feat (car search): develop a car-card design

--- car_resale_business_project/utils/filter_cars.py
+++ car_resale_business_project/utils/filter_cars.py
@@ -7,16 +7,12 @@ def filter_cars(filters, cars_data):
     }
 
     for filter_name, filter_value in filters.items():
-        print(f"filter_name = {filter_name}")
-        print(f"filter_value = {filter_value}")
-        print(f"filtered_cars before filter = {filtered_cars}")
         if filter_value is None or filter_value == "All":
             continue
         if "Date" in filter_name:
-            
             filter_function = filter_functions[filter_name]
             filtered_cars = [car for car in filtered_cars if filter_function(car, filter_value)]
         else:
             filtered_cars = [car for car in filtered_cars if car[filter_name] == filter_value]
-        print(f"filtered_cars after filter = {filtered_cars}")
+        
     return filtered_cars
